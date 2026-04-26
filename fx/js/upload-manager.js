1036bf7ad5069247284b58501606ea9e823f6337
jester8887 <76089566+jester8887@users.noreply.github.com>
2026-04-26 00:03:34 -0500

    Refactor init method in UploadManager

--- fx/js/upload-manager.js
+++ fx/js/upload-manager.js
@@ -1,10 +1,11 @@
 window.UploadManager = {
-    init() {
-      if (!DOM.uploadInput) return;
-    
-      DOM.uploadInput.addEventListener('click', () => {
-        DOM.uploadInput.value = "";
-      });
+  init() {
+    if (!DOM.uploadInput) return;
+
+    DOM.uploadInput.addEventListener('click', () => {
+      DOM.uploadInput.value = "";
+    });
+
     DOM.uploadInput.addEventListener('change', async (e) => {
       const file = e.target.files[0];
       if (!file) return;
